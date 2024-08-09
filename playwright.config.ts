e941e78c184b60db2c80a94d2c89a5cf290bec8e
StealthWorm <thierrypitela@hotmail.com>
2024-08-09 19:49:22 -0300

    fix: timeout test hendling

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http:/localhost:3000',
-    timeout: 120 * 1000,
+    timeout: 300 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
